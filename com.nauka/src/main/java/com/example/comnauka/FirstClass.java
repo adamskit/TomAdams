d05fce26a03da12bc08f1a6d089a13e3835ede02
Tomasz Adamski <tadamski@AURA.local>
2022-06-27 10:48:25 +0200

    First class

---
+++ com.nauka/src/main/java/com/example/comnauka/FirstClass.java
@@ -0,0 +1,4 @@
+package com.example.comnauka;
+
+public class FirstClass {
+}
---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.sql.SQLOutput;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
